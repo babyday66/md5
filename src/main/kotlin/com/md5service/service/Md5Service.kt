b67f39b4f2ec05229beb908ef31861023d6f9961
babyday66 <babyday66@gmail.com>
2026-06-18 07:39:41 +0300

    Make Md5Service and methods open for Spring

--- src/main/kotlin/com/md5service/service/Md5Service.kt
+++ src/main/kotlin/com/md5service/service/Md5Service.kt
@@ -4,9 +4,9 @@ import org.springframework.stereotype.Service
 import java.security.MessageDigest
 
 @Service
-class Md5Service {
+open class Md5Service {
 
-    fun calculateMd5(text: String): String {
+    open fun calculateMd5(text: String): String {
         val md = MessageDigest.getInstance("MD5")
         val messageDigest = md.digest(text.toByteArray())
         return messageDigest.joinToString("") { "%02x".format(it) }
