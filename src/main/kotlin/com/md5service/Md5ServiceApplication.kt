562a2454c936f6ea112b522a852f387746982f6d
babyday66 <babyday66@gmail.com>
2026-06-18 07:42:43 +0300

    Make Md5ServiceApplication class open for Spring

--- src/main/kotlin/com/md5service/Md5ServiceApplication.kt
+++ src/main/kotlin/com/md5service/Md5ServiceApplication.kt
@@ -4,7 +4,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class Md5ServiceApplication
+open class Md5ServiceApplication
 
 fun main(args: Array<String>) {
     runApplication<Md5ServiceApplication>(*args)
